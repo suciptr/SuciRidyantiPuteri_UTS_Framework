2b79afa90f4d41a5a37c951fb3fe4ce7adbdce1a
suciptr <148709994+suciptr@users.noreply.github.com>
2024-05-20 22:56:44 +0700

    services.php

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/callback',
+        'redirect' => 'https://suciputeri.simpangan.com/oauth/register/callback',
     ],
     
 ];
